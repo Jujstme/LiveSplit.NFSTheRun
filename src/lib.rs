e4f2acae351807fe5772cee0627a290a9056ffbe
Jujstme <just.tribe@gmail.com>
2023-02-04 03:06:42 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,27 @@
-use spinning_top::{Spinlock, const_spinlock};
+#![no_std]
 use asr::{
     timer,
     //timer::TimerState,
     watcher::Watcher,
-    Address, Process,
+    Address,
+    Process,
 };
+use spinning_top::{const_spinlock, Spinlock};
 
-static AUTOSPLITTER: Spinlock<State> = const_spinlock(
-    State {
-        game: None,
-        watchers: Watchers {
-            is_loading: Watcher::new(),
-            fade: Watcher::new(),
-        },
-        // settings: None,
-    }
-);    
+#[cfg(all(not(test), target_arch = "wasm32"))]
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    core::arch::wasm32::unreachable()
+}
+
+static AUTOSPLITTER: Spinlock<State> = const_spinlock(State {
+    game: None,
+    watchers: Watchers {
+        is_loading: Watcher::new(),
+        fade: Watcher::new(),
+    },
+    // settings: None,
+});
 
 struct State {
     game: Option<ProcessInfo>,
@@ -30,54 +36,62 @@ struct Watchers {
 }
 
 struct ProcessInfo {
-    game: Option<Process>,
+    game: Process,
     main_module_base: Address,
     //main_module_size: u64,
 }
 
 impl State {
     fn attach_process() -> Option<ProcessInfo> {
-        let game_name = "Need For Speed The Run.exe";
-        let game_process = Process::attach(&game_name);
+        //let game_name_2 = "whatever.exe";
+        //let game_process = Process::attach(game_name).or_else(|| Process::attach(game_name_2))?;
 
-        if game_process.is_none() {
-            return None;
+        //let game = Process::attach(game_name)?;
+
+        let game_names = ["Need For Speed The Run.exe"];
+
+        let mut proc: Option<Process> = None;
+        let mut curgamename: &str = "";
+        
+        for name in game_names {
+            let iproc = Process::attach(name);
+            if iproc.is_some() {
+                proc = iproc;
+                curgamename = name;
+                break;
+            }
         }
+        
+        let Some(game) = proc else { return None };
 
-        let Some(proc) = game_process else {
-            return None;
-        };
-
-        let game_main_module_base = proc.get_module_address(&game_name).ok().unwrap();
-        //let game_main_module_size = proc.get_module_size(&game_name).ok().unwrap();
+        let main_module_base = game.get_module_address(curgamename).ok()?;
 
         Some(ProcessInfo {
-            game: Some(proc),
-            main_module_base: game_main_module_base,
+            game,
+            main_module_base,
             //main_module_size: game_main_module_size,
         })
     }
 
-    fn check_if_attached(&self) -> bool {
-        if self.game.is_none() {
-            return false;
-        }
-
-        let Some(game) = &self.game else { return false };
-        let Some(proc) = &game.game else {return false};
-        proc.is_open()
-    }
-
     fn update(&mut self) {
         // Attaching to target process
-        if !self.check_if_attached() { self.game = State::attach_process() }
-        if !self.check_if_attached() { return; }
+        if self.game.is_none() {
+            self.game = State::attach_process()
+        }
 
-        // Updating the watchers
-        let game = self.game.as_ref().unwrap().game.as_ref().unwrap();
-        let Address(base_address) = self.game.as_ref().unwrap().main_module_base;
-        let is_loading = self.watchers.is_loading.update(game.read(Address(base_address + 0x24682E4)).ok()).unwrap();
-        let fade = self.watchers.fade.update(game.read(Address(base_address + 0x248BBC4)).ok()).unwrap();
+        let Some(game) = &self.game else {
+            return;
+        };
+        let proc = &game.game;
+        if !proc.is_open() {
+            self.game = None;
+            return;
+        };
+
+        // Update
+        let Address(base_address) = game.main_module_base;
+        let Some(is_loading) = self.watchers.is_loading.update(proc.read(Address(base_address + 0x24682E4)).ok()) else { return };
+        let Some(fade) = self.watchers.fade.update(proc.read(Address(base_address + 0x248BBC4)).ok()) else { return };
 
         if is_loading.current == 0 {
             timer::resume_game_time()
